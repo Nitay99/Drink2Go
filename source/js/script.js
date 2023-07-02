6e2a61cf76b70d631b26de29a17a8dc9cf68a6d2
Nitay <trakktttor@gmail.com>
2023-07-02 21:23:45 +0700

    Завершает проект.

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,40 @@
-// let navMain = document.querySelector('.main-nav');
-// let navToggle = document.querySelector('.main-nav__toggle');
+/* Menu */
 
-// navMain.classList.remove('main-nav--nojs');
+const navMain = document.querySelector('.main-nav');
+const navToggle = document.querySelector('.main-nav__toggle');
 
-// navToggle.addEventListener('click', function () {
-//   if (navMain.classList.contains('main-nav--closed')) {
-//     navMain.classList.remove('main-nav--closed');
-//     navMain.classList.add('main-nav--opened');
-//   } else {
-//     navMain.classList.add('main-nav--closed');
-//     navMain.classList.remove('main-nav--opened');
-//   }
-// });
+navMain.classList.remove('main-nav--nojs');
+
+const setMenuClickHandler = () => {
+  navToggle.addEventListener('click', function () {
+    if (navMain.classList.contains('main-nav--closed')) {
+      navMain.classList.remove('main-nav--closed');
+      navMain.classList.add('main-nav--opened');
+    } else {
+      navMain.classList.add('main-nav--closed');
+      navMain.classList.remove('main-nav--opened');
+    }
+  });
+}
+
+setMenuClickHandler();
+
+/* Slider */
+
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
